bd88b7fef59a0f7966aee8d950bd71c76c2b3065
khanhngg <khanhnguye94@gmail.com>
2017-05-02 01:01:15 -0700

    Initial hw8 template

---
+++ src/HashedEntry.h
@@ -0,0 +1,30 @@
+#pragma  once
+
+#include "Entry.h"
+
+template<class KeyType, class ItemType>
+class HashedEntry : public Entry<KeyType, ItemType> {
+private:
+    HashedEntry<KeyType, ItemType> *nextPtr;
+
+public:
+    HashedEntry();
+
+    HashedEntry(KeyType searchKey, ItemType newEntry);
+
+    HashedEntry(KeyType searchKey, ItemType newEntry,
+                HashedEntry<KeyType, ItemType> *nextEntryPtr);
+
+    void setNext(HashedEntry<KeyType, ItemType> *nextEntryPtr);
+
+    HashedEntry<KeyType, ItemType> *getNext() const;
+
+    void operator=(const ItemType&);
+};
+
+template<class KeyType, class ItemType>
+void HashedEntry<KeyType, ItemType>::operator=(const ItemType& newItem){
+    //DO NOT MODIFY
+    this->setItem(newItem);
+
+};
